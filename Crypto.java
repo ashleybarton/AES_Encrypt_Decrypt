d93d981b3cd7e44547ec324d2fef886b9cc92a64
Ashley Barton <barton.ash@gmail.com>
2014-04-29 04:09:02 -0700

    Completed project. Project requirements met

--- Crypto.java
+++ Crypto.java
@@ -3,16 +3,19 @@
 
 import java.security.*;
 import javax.crypto.*;
+import org.apache.commons.codec.binary.Base64;
 
 public class Crypto {
     private String plaintext;
+    private String ciphertext;
+    private String decryptedText;
     private KeyGenerator kg;
     private SecretKey sk;
     private Cipher c;
 
     public Crypto(String msg) {
         plaintext = msg;
-        System.out.println("Plaintext message: " + plaintext);
+        System.out.println("\nPlaintext: " + plaintext + "\n");
     }
 
     //generate the key
@@ -21,7 +24,6 @@ public class Crypto {
             kg = KeyGenerator.getInstance("AES");
             kg.init(128);
             sk = kg.generateKey();
-            System.out.println("Generating the key.");
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
@@ -29,36 +31,57 @@ public class Crypto {
 
     //sets the cipher type to AES
     public void setCipher() {
-        System.out.println("Setting AES as the encryption type."); 
         try{     
             c = Cipher.getInstance("AES");
             c.init(Cipher.ENCRYPT_MODE, sk);
-        }   catch (NoSuchAlgorithmException noSuchAlgo) {
-            System.out.println(" No Such Algorithm exists " + noSuchAlgo);
+        }   catch (NoSuchAlgorithmException e) {
+            System.out.println("No Such Algorithm Exists");
             }
-            catch (NoSuchPaddingException noSuchPad) {
-                System.out.println(" No Such Padding exists " + noSuchPad);
+            catch (NoSuchPaddingException e) {
+                System.out.println("No Such Padding Exists");
             }
             catch (InvalidKeyException invalidKey){
-                System.out.println(" Invalid Key " + invalidKey);
+                System.out.println("Invalid Key");
             }
     }
-/*
-            catch (NoSuchPaddingException noSuchPad) {
-                System.out.println(" No Such Padding exists " + noSuchPad);
+    
+    public String encrypt() {
+        try{
+            byte [] bytesOfPlaintext = plaintext.getBytes();
+            byte [] bytesOfCiphertext = c.doFinal(bytesOfPlaintext);
+            ciphertext = new String(Base64.encodeBase64(bytesOfCiphertext));
+            
+            System.out.println("ciphertext: " + ciphertext + "\n\n");
+        } 
+            catch (BadPaddingException e){
+                System.out.println("Bad padding");
             }
-            catch (InvalidKeyException invalidKey){
-                System.out.println(" Invalid Key " + invalidKey);
+            catch (IllegalBlockSizeException illegalblocksize) {
+                System.out.println("Illegal block size");
             }
-            catch (BadPaddingException badPadding){
-                System.out.println(" Bad Padding " + badPadding);
-            }
-            catch (IllegalBlockSizeException illegalBlockSize) {
-                System.out.println(" Illegal Block Size " + illegalBlockSize);
-            }
-            catch (InvalidAlgorithmParameterException invalidParam) {
-                System.out.println(" Invalid Parameter " + invalidParam);
-            }
+        return ciphertext;    
+    }
 
-//*/
+    public String decrypt(String ct) {
+        try{
+              byte [] bytesOfCiphertext = Base64.decodeBase64(ct.getBytes());
+                
+                c.init(Cipher.DECRYPT_MODE, sk, c.getParameters());
+                byte [] bytesDecrypted = c.doFinal(bytesOfCiphertext);
+                decryptedText = new String(bytesDecrypted);
+        }
+            catch (BadPaddingException e){
+                System.out.println("Padding issue");
+            }
+            catch (IllegalBlockSizeException e) {
+                System.out.println("Illegal Block Size");
+            }
+            catch (InvalidKeyException e){
+                System.out.println("Invalid Key");
+            }
+            catch (InvalidAlgorithmParameterException e) {
+                System.out.println("Invalid parameter");
+            }
+        return decryptedText;
+    }
 }
--- HW2.java
+++ HW2.java
@@ -7,20 +7,33 @@ import java.util.Scanner;
 public class HW2 {
 
     public static void main(String [] args) {    
-
+        String ciphertext;
+        String decrypted;
         String plaintext = getText("plaintext.txt");
-        System.out.println(plaintext);
-
         Crypto aes = new Crypto(plaintext);
 
+        //Encrypt the message
         aes.genKey();
         aes.setCipher();
+        ciphertext = aes.encrypt();
+        
+        //store the ciphertext into a file
+        writeToFile(ciphertext, "ciphertext.txt");
+        
+        //read the contents of ciphertext
+        String ciphertextFromFile = getText("ciphertext.txt");
+        System.out.println("Cipher text from file: " + ciphertextFromFile + "\n\n"); 
+        
+        decrypted = aes.decrypt(ciphertextFromFile);
+
+        //Write Decrypted message to decryptedtext.txt
+        writeToFile(decrypted, "decryptedtext.txt");
+        System.out.println("Decrypted Message: " + decrypted + "\n\n");
     }
 
     public static String getText(String filename) {
         String text = null;
         File file = new File(filename);
-        
         //Read the message from given file
         try {
                 Scanner scan = new Scanner(file);
