7572e48a533eb3545149c4307ef41193c0e80bd6
Ashley Barton <barton.ash@gmail.com>
2014-04-28 22:29:32 -0700

    renaming files

---
+++ HW2.java
@@ -0,0 +1,55 @@
+//Ashley Barton
+//Homework 2 - Problem 4
+//April, 20th 2014
+import java.io.*;
+import java.util.Scanner;
+
+public class HW2 {
+
+    public static void main(String [] args) {    
+
+        String plaintext = getText("plaintext.txt");
+        System.out.println(plaintext);
+
+        writeToFile(plaintext, "ciphertext.txt");
+        String foo = getText("ciphertext.txt");
+        System.out.println(foo);
+        
+    }
+
+    public static String getText(String filename) {
+        String text = null;
+        File file = new File(filename);
+        
+        //Read the message from given file
+        try {
+                Scanner scan = new Scanner(file);
+                text = scan.nextLine();
+                scan.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return text;
+    }
+
+    public static void writeToFile(String str, String filename) {
+        File f = new File(filename);
+
+        if(f.isFile() == false) {
+            try {
+                f.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+                PrintWriter outFile = new PrintWriter(filename);
+                outFile.println(str);
+                outFile.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+    }
+
+}
